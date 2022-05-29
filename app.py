4221ca04c6a76fe71e0f27322377dd71e6d96262
Jose Salvatierra <jslvtr@gmail.com>
2022-05-29 16:12:55 +0100

    📦️ (OpenAPI) Update OpenAPI version in projects to 3.0.3

--- app.py
+++ app.py
@@ -11,17 +11,17 @@ from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
 
 
-def create_app(db_uri=None):
+def create_app(db_url=None):
     app = Flask(__name__)
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
-    app.config["OPENAPI_VERSION"] = "3.0.2"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_uri or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
